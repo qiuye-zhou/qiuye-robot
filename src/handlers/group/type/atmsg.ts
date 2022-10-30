64423b3fd1a68187eb4a7afe4d74a59bb523c783
qiuye <1302580865@qq.com>
2022-10-30 10:12:54 +0800

    style

--- src/handlers/group/type/atmsg.ts
+++ src/handlers/group/type/atmsg.ts
@@ -8,7 +8,7 @@ import { AtElem } from "oicq";
 export const AtMessage: GroupOnionRoutine = async function (event) {
     const messages = event.message
     const atMessageIndex = messages.findIndex((e) => {
-        if ((e as AtElem).qq ===RobotConfig.Uid) {
+        if ((e as AtElem).qq === RobotConfig.Uid) {
             return e.type === 'at'
         }
     })
