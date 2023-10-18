118a85242da606e00cce28e2fb8369dad77e7bdf
qiuye <1302580865@qq.com>
2023-10-18 14:43:53 +0800

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -20,6 +20,24 @@ client.on('system.login.qrcode', (e) => {
     })
 })
 
+client.on('system.login.device', (e) => {
+    console.log('请选择验证方式:(1：短信验证   其他：扫码验证)')
+    process.stdin.once('data', (data) => {
+        if (data.toString().trim() === '1') {
+            client.sendSmsCode()
+            console.log('请输入手机收到的短信验证码:')
+            process.stdin.once('data', (res) => {
+                client.submitSmsCode(res.toString().trim())
+            })
+        } else {
+            console.log('扫码完成后回车继续：' + e.url)
+            process.stdin.once('data', () => {
+                client.login()
+            })
+        }
+    })
+})
+
 client.login(RobotConfig.Uid, RobotConfig.password)
 
 client.on('message.private', function (e) {
